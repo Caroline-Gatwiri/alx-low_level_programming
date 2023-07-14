1c5042ca62624893ff23bf562d25dc6e07b0f21e
Caroline-Gatwiri <caroline1gatwiri@gmail.com>
2023-07-13 19:09:25 -0700

    Hello puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
- * main - A program that prints a line with put function
- * return : 0 (Success)
+ * main - A program that prints a line with the `puts` function
+ * Return: 0 (Success)
  */
 int main(void)
 {
