b41535c2dfef1c145c3c2e88accd76c0d68c828a
Caroline-Gatwiri <caroline1gatwiri@gmail.com>
2023-07-20 07:19:06 -0700

    task 11

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - looks for and prints largest prime factors
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	long int num = 612852475143, divisor = 4;
+
+	while (divisor < (num / 2))
+	{
+		if ((num % 2) == 0)
+		{
+			num /= 2;
+			continue;
+		}
+		for (divisor = 3; divisor < (num / 2); divisor += 2)
+		{
+			if ((num % divisor) == 0)
+				num /= divisor;
+		}
+
+	}
+	printf("%ld\n", num);
+	return (0);
+}
+
